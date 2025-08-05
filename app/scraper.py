329fba9c591df26816d02f2f4b108ca5a3cd0871
Samuel Alexander <samuelalexander0021@gmail.com>
2025-08-05 10:41:43 +0530

    add docker compose

--- app/scraper.py
+++ app/scraper.py
@@ -15,6 +15,9 @@ def scrape_court_cases(case_type, case_number, filling_year):
         options = webdriver.ChromeOptions()
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--headless')  # Run in headless mode for Docker
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920,1080')
         
        
         driver = webdriver.Chrome(options=options)
